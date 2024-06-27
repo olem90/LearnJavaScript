77fbd1f3f8e108a2746bdea2efe4e8a951fe03fd
olem90 <olemariusrognan90@gmail.com>
2024-06-27 17:14:49 +0100

    Started adding content to the Lessons page. Created an Index. and added some stylingg

--- src/components/Lessons/Lessons.styles.jsx
+++ src/components/Lessons/Lessons.styles.jsx
@@ -2,16 +2,76 @@ import styled from "styled-components";
 
 export const LessonsWrapper = styled.div`
     box-sizing: border-box;
-    height: 100vh;
-    width: 100vw;
-    border: 1px solid transparent;
+    min-height: 100vh;
+    min-width: 100%; 
+    border: 1px solid transparent; 
+    background-color: #FAFAFA;   
 
     h1 {
-        color: #F5F5F5;
+        color: #333;
         margin-inline: auto;
-        padding: 8px;
-        width: fit-content;
+        padding: 8px; 
+        width: fit-content; 
         margin-top: 80px;
     }
+`;
+
+export const LessonsContainer = styled.div`   
+    display: flex; 
+    flex-direction: row;    
+    max-width: 86%;   
+    margin-inline: auto; 
 `
+
+export const Sidebar = styled.div`
+    flex: 1;  
+    background-color: #f4f4f4; 
+    padding: 20px;
+    box-shadow: 2px 0 5px rgba(0,0,0,0.1);
+    height: 100%;
+    overflow-y: auto;
  
+    h2 {
+        color: #333;
+    } 
+
+    a {
+        display: block;
+        color: #333;
+        text-decoration: none;
+        padding: 10px 0;
+    }
+
+    a:hover {
+        background-color: #ddd;
+    }
+`;
+
+export const Content = styled.div`
+    margin-left: 10px;
+    padding: 20px; 
+    flex: 4;   
+
+    h2 {
+        color: #333;    
+    }
+
+    code {
+        background-color: #eee; 
+        padding: 2px 4px;
+    }
+
+    pre {
+        background-color: #eee;
+        padding: 10px;
+        border-radius: 4px;
+        overflow-x: auto;
+        white-space: pre-wrap; /* This ensures the text wraps and doesn't overflow the container */
+    }
+`;
+
+export const ContentContainer = styled.div`
+    max-width: 800px; 
+    margin-inline: auto;  
+` 
+   
